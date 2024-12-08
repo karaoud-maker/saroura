1852a05a51051300676dd210b0399b2f4d2a991b
karaoud-maker <rayenkaraood@gmail.com>
2024-12-08 09:24:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('YOU DESERVE FLOWERS THAT NEVER FADE,JUST LIKE THE SPARKLE IN YOUR EYES').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
